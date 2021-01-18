65b073db9b2b2d94c010ac6c89e023ae084c71fa
marcinmmalec <martinmmalec@gmail.com>
2021-01-18 10:53:27 -0700

    Fixed client side/Added logic to client side

--- server/client/src/components/ListItem/ListItem.jsx
+++ server/client/src/components/ListItem/ListItem.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import {ListSubItem} from '../ListSubItem';
+//import {DateTime} from 'luxon';
 
-const ListItem = ({data}) => {
+const ListItem = ({id, data}) => {
     return (
-        <tbody>
-            <tr>
-                <ListSubItem data={data.name}/>
-                <ListSubItem data={data.departure}/>
-                <ListSubItem data={data.arrival}/>
-                <ListSubItem data={data.fare}/>
-            </tr>
-        </tbody>
+        <tr id={id}>
+            {data.map((x, index)=> (
+                <ListSubItem key={index} data={x} />
+            ))}
+        </tr>
   );
 }
 
 export default ListItem;
+
+/*<ListSubItem data={data.name}/>
+                <ListSubItem data={DateTime.fromObject({hour: data.departureHour, minute: data.departureMinute}).toLocaleString(DateTime.TIME_SIMPLE)}/>
+                <ListSubItem data={DateTime.fromObject({hour: data.arrivalHour, minute: data.arrivalMinute}).toLocaleString(DateTime.TIME_SIMPLE)}/>
+                <ListSubItem data={data.fare}/>*/
--- server/client/src/views/RoutesList/RoutesList.js
+++ server/client/src/views/RoutesList/RoutesList.js
@@ -1,21 +1,24 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import axios from 'axios';
+import {DateTime} from 'luxon';
 import {ListItem} from '../../components/ListItem';
 import './RoutesList.css';
-import {DateTime} from 'luxon';
 
 class RoutesList extends Component {
     constructor() {
         super();
         this.state = {
-            routes: []
+            routes: [],
+            time: 0
         }
         this.getRoutes = this.getRoutes.bind(this);
     }
     
     componentDidMount() {
         this.getRoutes();
+        this.setState({time: new Date()});
+        console.log(this.state.time.getHour);
     }
     
     async getRoutes() {
@@ -27,6 +30,7 @@ class RoutesList extends Component {
                 console.log(error);
             });
         this.setState({routes: data.data});
+        console.log(this.state.routes);
     }
     
     render() {
@@ -42,10 +46,12 @@ class RoutesList extends Component {
                             <th>Fare</th>
                         </tr>
                     </thead>
-                    {this.state.routes.map((route, index) => (
-                        <ListItem key={route.id} data={route}/>
+                    <tbody>
+                        {this.state.routes.filter(route => DateTime.fromObject({hour: route.departureHour, minute: route.departureMinute}) > DateTime.fromObject({hour: this.state.time.getHours(), minute: this.state.time.getMinutes()})).sort((a, b) => a.departureHour-b.departureHour || a.departureMinute-b.departureMinute).filter((route, index, self) => index === self.findIndex((t) => (t.name === route.name))).map((route, index) => (
+                        <ListItem key={route._id} id={route._id} data={[route.name, (DateTime.fromObject({hour: route.departureHour, minute: route.departureMinute}).toLocaleString(DateTime.TIME_SIMPLE)), (DateTime.fromObject({hour: route.arrivalHour, minute: route.arrivalMinute}).toLocaleString(DateTime.TIME_SIMPLE)), route.fare]}/>
                         //<li key={route.id}>{route.name} {route.departureHour}:{route.departureMinute} {route.arrivalHour}:{route.arrivalMinute}</li>
                     ))}
+                    </tbody>
                 </table>
             </div>
         );
@@ -55,7 +61,10 @@ class RoutesList extends Component {
 export default RoutesList;
 
 
-
+/*{this.state.routes.map((route, index) => (
+                        <ListItem key={route._id} data={route}/>
+                        //<li key={route.id}>{route.name} {route.departureHour}:{route.departureMinute} {route.arrivalHour}:{route.arrivalMinute}</li>
+                    ))}*/
 
 
 //    <div style={{ display: "flex", flexDirection: "column", alignContent: "center",  backgroundColor:"orange"}}>
