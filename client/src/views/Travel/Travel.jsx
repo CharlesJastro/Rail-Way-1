4d6e8220e8508e603f85b6910174c09ec23f1455
HmingPuia <sawmasn7@gmail.com>
2021-02-08 12:44:49 -0700

    Add Comment Map Code

--- client/src/views/Travel/Travel.jsx
+++ client/src/views/Travel/Travel.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+//import GoogleMap from '../../components/GoogleMap';
 
 const Travel = props => {
   return (
     <div>
       <h3>Travel View</h3>
+      {/* <GoogleMap/> */}
     </div>
   );
 };
