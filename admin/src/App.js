05f937a24474e61a665e0ac8996017254ed4d5fd
HmingPuia <sawmasn7@gmail.com>
2021-02-09 15:27:43 -0700

    Adding Admin Stations

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,13 +1,15 @@
 import AdminExceptions from "./components/AdminExceptions";
 import AdminNotifications from "./components/AdminNotifications";
 import AdminRoutes from "./components/AdminRoutes";
+import AdminStations from "./components/AdminStations";
 
 function App(){
   return(
-    <div className="container">
+    <div className="ui container">
       <AdminNotifications/>
       <AdminRoutes/>
       <AdminExceptions/>  
+      <AdminStations/>
     </div>
   )
 }
