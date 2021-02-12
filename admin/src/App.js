64d186f464f00eca766e131e3c76f9dcea43adb3
Dzhwar87 <dzhar>
2021-02-12 00:20:52 -0700

    NotFound.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,15 +2,32 @@ import AdminExceptions from "./components/AdminExceptions";
 import AdminNotifications from "./components/AdminNotifications";
 import AdminRoutes from "./components/AdminRoutes";
 import AdminStations from "./components/AdminStations";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NotFound from "./components/NotFound";
 
 function App(){
   return(
+    <Router>
     <div className="ui container">
-      <AdminNotifications/>
+      <Switch>
+        <Route exact path="/adminnotifications">
+        <AdminNotifications/>
+        </Route>
+        <Route path="/adminroutes">
       <AdminRoutes/>
-      <AdminExceptions/>  
+      </Route>
+      <Route path="/adminexception">
+      <AdminExceptions/> 
+      </Route> 
+      <Route path="/adminstations">
       <AdminStations/>
+      </Route>
+      <Route path="*">
+        <NotFound/>
+      </Route>
+      </Switch>
     </div>
-  )
+    </Router>
+  );
 }
 export default App;
