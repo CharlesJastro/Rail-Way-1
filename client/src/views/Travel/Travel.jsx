e89e39c7f6a87a36e11f37b62257364bc0aa00bb
CharlesJastro <olujastro@gmail.com>
2021-02-15 13:29:11 -0700

    Still Ongoing Travel Page

--- client/src/views/Travel/Travel.jsx
+++ client/src/views/Travel/Travel.jsx
@@ -1,40 +1,139 @@
 import React, { Component } from 'react';
 //import { makeStyles } from '@material-ui/core/styles';
-import {TravelStations} from './TravelStations';
+import { TravelStations } from './TravelStations';
 import Calendar from 'react-calendar'
 //import GoogleMap from '../../components/GoogleMap';
-import { Button , Icon } from 'semantic-ui-react'
+import { Button, Icon } from 'semantic-ui-react'
+import { useState } from 'react';
+import Modal from 'react-modal';
+import axios from 'axios';
+
+
+// Modal Dialog function component to open stationList
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
+
+// Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
+// Modal.setAppElement('#root')
+
+
+//Calender hide and show function component  
+function MyApp(props) {
+  const [value, onChange] = useState(new Date());
+  console.log(value);
+
+  if (props.status) {
+    return (
+      <div>
+        <Calendar
+          onChange={onChange}
+          value={value}
+        />
+      </div>
+    );
+  }
+  else {
+    return (
+      <div></div>
+    )
+  }
+}
+
 
 class Travel extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      myCalender: new Date(),
+      status: false,
+      modalIsOpen: false,
+      uniqueStationList: []
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+    this.openModal = this.openModal.bind(this);
+
+  };
+
+  handleClick() {
+    this.setState(state => ({
+      status: !state.status
+    }));
+  }
+
+  openModal() {
+    this.setState(state => ({
+      modalIsOpen: !state.modalIsOpen
+    }))
+  }
+
+  componentDidMount() {
+    axios
+      .get('/stations/list/')
+      .then((response) => {
+        this.setState(state => ({
+          uniqueStationList: response.data
+        }));
+      })
+  }
+  //  afterOpenModal() {
+  //   // references are now sync'd and can be accessed.
+  //   subtitle.style.color = '#f00';
+  // }
+
+  //  closeModal() {
+  //     setIsOpen(false);
+  //   }
+
   render() {
+    console.log(this.state.myCalender);
+    console.log(this.state.uniqueStationList);
     return (
       <div>
-         <h3>Travel View</h3>
-<<<<<<< HEAD
-         <Button onClick={()=>window.location.href = "./travelstations" }>From:     Station</Button> <hr/>
-         <Button onClick={()=>window.location.href = "./travelstations" }>To:     Station</Button> <hr/>
-         <Button onClick={()=>window.location.href = './travel' }>Depart At:     Now</Button> <hr/> <br/><br/><br/>
-         <Calendar />
-         <Button variant="contained">Find</Button><br/><br/><br/><br/>
-=======
-         <Button onClick={()=>window.location.href = "./travelstations" }>From:  Station</Button> <hr/>
-         <Button>To:  Station</Button> <hr/>
-         <Button>Depart At:  Now</Button> <hr/> <br/><br/><br/>
-         <Button animated>
-           <Button.Content visible>Find</Button.Content>
-           <Button.Content hidden>
-             <Icon name='arrow down' />
-           </Button.Content>
-           </Button>
-         <br/><br/><br/><br/>
->>>>>>> 81d0d0a7a0474a67cff9e0f872fe6f83a9a9999a
-         
+        <h3>Travel View</h3>
+        <Button onClick={() => window.location.href = "./travelstations"}>From:  Station</Button> <hr />
+        <Button onClick={this.openModal}>To:  Station</Button> <hr />
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <button onClick={this.openModal}>X</button>
+          <form>
+            <h4>Choose Station</h4>
+            {this.state.uniqueStationList.map((station) => (
+              <div>{station}</div>
+            ))}
+          </form>
+        </Modal>
 
-         {/* <Button color="primary">To:    Station</Button> <hr/>
+        <Button onClick={this.handleClick}>Depart At:  Now</Button> <hr /> <br /><br /><br />
+
+        <MyApp status={this.state.status} />
+
+        <Button animated>
+          <Button.Content visible>Find</Button.Content>
+          <Button.Content hidden>
+            <Icon name='arrow down' />
+          </Button.Content>
+        </Button>
+        <br /><br /><br /><br />
+
+
+        {/* <Button color="primary">To:    Station</Button> <hr/>
          <Button color="secondary">Depart At:    Now</Button> <hr/>
          <Button disabled>Disabled</Button>
-         <Button href="#text-buttons" color="primary">Link</Button> */} 
+         <Button href="#text-buttons" color="primary">Link</Button> */}
       </div>
     )
   }
--- client/src/views/Travel/TravelStations.jsx
+++ client/src/views/Travel/TravelStations.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-//import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-=======
 import Button from '@material-ui/core/Button'
->>>>>>> 81d0d0a7a0474a67cff9e0f872fe6f83a9a9999a
+import { useState } from 'react';
+import Modal from 'react-modal';
+
+
 
 
 class TravelStations extends Component {
@@ -12,18 +11,13 @@ class TravelStations extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
-         <h3>Travel View</h3>
-         <Button variant="contained" >My Favourites</Button>
-=======
          <h3>Travel View</h3> 
         
       <Button variant="contained">My Favourites</Button>
     
->>>>>>> 81d0d0a7a0474a67cff9e0f872fe6f83a9a9999a
          <Button variant="contained">From Map</Button> <hr/> <br/><br/><br/>
          <h3>Travel View</h3>
-         <Button onClick={()=>window.location.href = "./travelstations" }> Abuja (Idu)   </Button> <hr/>
+         <Button>Abuja (Idu)   </Button> <hr/>
          <Button>ABUJA (Kubwa)       </Button> <hr/>
          <Button>Kaduna (Rigasa)     </Button> <hr/> <br/><br/><br/>
          
