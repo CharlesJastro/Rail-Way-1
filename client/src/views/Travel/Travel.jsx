35fbba2cf894b34333129e07db681a3f28da50cd
marcinmmalec <martinmmalec@gmail.com>
2021-02-17 00:57:59 -0700

    Refractor Travel.jsx
    Please refer to Project Update: Feb 16

--- client/src/views/Travel/Travel.jsx
+++ client/src/views/Travel/Travel.jsx
@@ -1,144 +1,158 @@
 import React, { Component } from 'react';
-//import { makeStyles } from '@material-ui/core/styles';
 import { TravelStations } from './TravelStations';
-import Calendar from 'react-calendar'
 //import GoogleMap from '../../components/GoogleMap';
-import { Button, Icon } from 'semantic-ui-react'
+import { Button, Icon, Modal, Form, Input} from 'semantic-ui-react';
 import { useState } from 'react';
-import Modal from 'react-modal';
 import axios from 'axios';
+import {DateTime} from 'luxon';
+import TimePicker from 'react-time-picker';
+import styleReset from './styleReset.css';
 
-
-// Modal Dialog function component to open stationList
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)'
-  }
-};
-
-// Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
-// Modal.setAppElement('#root')
-
-
-//Calender hide and show function component  
-function MyApp(props) {
-  const [value, onChange] = useState(new Date());
-  console.log(value);
-
-  if (props.status) {
-    return (
-      <div>
-        <Calendar
-          onChange={onChange}
-          value={value}
-        />
-      </div>
-    );
-  }
-  else {
-    return (
-      <div></div>
-    )
-  }
-}
-
-
-class Travel extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      myCalender: new Date(),
-      status: false,
-      modalIsOpen: false,
-      uniqueStationList: []
+const Travel = () => {
+    const [uniqueStationList, setUniqueStationList] = React.useState([]);
+    const [day, setDay] = React.useState(DateTime.fromJSDate(new Date()).toLocaleString({weekday: 'long'}));
+    const [time, setTime] = React.useState(DateTime.fromJSDate(new Date()));
+    const [timeHour, setTimeHour] = React.useState();
+    const [timeMinute, setTimeMinute] = React.useState();
+    const [fromStation, setFromStation] = React.useState('Station');
+    const [toStation, setToStation] = React.useState('Station');
+    const [dateOption, setDateOption] = React.useState(0);
+    const [openToModal, setOpenToModal] = React.useState(false);
+    const [openFromModal, setOpenFromModal] = React.useState(false);
+    const [openDateModal, setOpenDateModal] = React.useState(false);
+    
+    React.useEffect(() => {
+        async function fetchData () {
+            let data;
+            try {
+                data = await axios
+                    .get('/stations/list/')
+                setUniqueStationList(data.data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        let dt = DateTime.fromJSDate(new Date());
+        fetchData();
+    }, []);
+    
+    React.useEffect(() => {
+        console.log(day);
+        console.log(time);
+    }, [day, time])
+    
+    function showList(type) {
+        try {
+            if (type === 'from') {
+                return (
+                    uniqueStationList.map((station, index) => (
+                        <div key={station+index}>
+                            <Button 
+                               onClick={() => [setFromStation(station), setOpenFromModal(false)]}
+                            >
+                                {station}
+                            </Button>
+                            <br /> <br/>
+                        </div>
+                    ))
+                );
+            } else {
+                return (
+                    uniqueStationList.map((station, index) => (
+                        <div key={station+index}>
+                            <Button 
+                               onClick={() => [setToStation(station), setOpenToModal(false)]}
+                            >
+                                {station}
+                            </Button>
+                            <br /> <br/>
+                        </div>
+                    ))
+                );
+            }
+            
+        } catch (error) {
+            console.log(error);
+        }
     }
 
-    this.handleClick = this.handleClick.bind(this);
-    this.openModal = this.openModal.bind(this);
-
-  };
-
-  handleClick() {
-    this.setState(state => ({
-      status: !state.status
-    }));
-  }
-
-  openModal() {
-    this.setState(state => ({
-      modalIsOpen: !state.modalIsOpen
-    }))
-  }
-
-  componentDidMount() {
-    axios
-      .get('/stations/list/')
-      .then((response) => {
-        this.setState(state => ({
-          uniqueStationList: response.data
-        }));
-      })
-  }
-  //  afterOpenModal() {
-  //   // references are now sync'd and can be accessed.
-  //   subtitle.style.color = '#f00';
-  // }
-
-  //  closeModal() {
-  //     setIsOpen(false);
-  //   }
-
-  render() {
-    console.log(this.state.myCalender);
-    console.log(this.state.uniqueStationList);
     return (
-      <div>
-        <h3>Travel View</h3>
-        <Button onClick={this.openModal}>From:  Station</Button> <hr />
-        <Button onClick={this.openModal}>To:  Station</Button> <hr />
-        <Modal
-          isOpen={this.state.modalIsOpen}
-          style={customStyles}
-          contentLabel="Example Modal"
-        >
-          <button onClick={this.openModal}>X</button>
-          <form>
-            <h4>Choose Station</h4>
-            {this.state.uniqueStationList.map((station) => (
-              <div>{station}</div>
-            ))}
-          </form>
-        </Modal>
-
-        <Button onClick={this.handleClick}>Depart At:  Now</Button> <hr /> <br /><br /><br />
-
-        <MyApp status={this.state.status} />
-
-        <Button animated>
-          <Button.Content visible>Find</Button.Content>
-          <Button.Content hidden>
-            <Icon name='arrow down' />
-          </Button.Content>
-        </Button>
-        <br /><br /><br /><br />
-
-
-        {/* <Button color="primary">To:    Station</Button> <hr/>
-         <Button color="secondary">Depart At:    Now</Button> <hr/>
-         <Button disabled>Disabled</Button>
-         <Button href="#text-buttons" color="primary">Link</Button> */}
-      </div>
-    )
-  }
+        <div>
+            <h3>Travel View</h3>
+            <Modal
+                open={openFromModal}
+                trigger={<Button >From: {fromStation}</Button>}
+                onClose={() => setOpenFromModal(false)}
+                onOpen={() => setOpenFromModal(true)}
+            >
+                <Modal.Header>Select Station</Modal.Header>
+                <Modal.Content>
+                    {showList('from')}
+                </Modal.Content>
+            </Modal>
+            <hr />
+            <Modal
+                open={openToModal}
+                trigger={<Button >To: {toStation}</Button>}
+                onClose={() => setOpenToModal(false)}
+                onOpen={() => setOpenToModal(true)}
+            >
+                <Modal.Header>Select Station</Modal.Header>
+                <Modal.Content>
+                    {showList('to')}
+                </Modal.Content>
+            </Modal>
+            <hr />
+            <Modal
+                closeIcon
+                open={openDateModal}
+                trigger={<Button >Depart At: {day} at {time.toLocaleString(DateTime.TIME_SIMPLE)}</Button>}
+                onClose={() => setOpenDateModal(false)}
+                onOpen={() => setOpenDateModal(true)}
+            >
+                <Modal.Header>Select Day and Time</Modal.Header>
+                <Modal.Content>
+                    <Form>
+                        <Form.Field>
+                            <label htmlFor="day">Day</label>
+                            <select id="day" value={day} onChange={(e) => {
+                                setDay(e.target.value);
+                            }}>
+                                <option value={'Sunday'}>Sunday</option>
+                                <option value={'Monday'}>Monday</option>
+                                <option value={'Tuesday'}>Tuesday</option>
+                                <option value={'Wednesday'}>Wednesday</option>
+                                <option value={'Thursday'}>Thursday</option>
+                                <option value={'Friday'}>Friday</option>
+                                <option value={'Saturday'}>Saturday</option>
+                            </select>
+                        </Form.Field>
+                        <Form.Field>
+                            <label htmlFor="time">Time</label>
+                            <TimePicker style={styleReset} id="time" value={time.toLocaleString(DateTime.TIME_24_SIMPLE)} onChange={(e)=>{
+                                setTime(DateTime.fromISO(e));
+                            }}/>
+                            <br />
+                            <br />
+                            <Button primary onClick={()=>setOpenDateModal(false)}>Confirm</Button>
+                            <Button secondary onClick={()=>[setDay(DateTime.fromJSDate(new Date()).toLocaleString({weekday: 'long'})), setTime(DateTime.fromJSDate(new Date()))]}>Reset</Button>
+                        </Form.Field>
+                    </Form>
+                </Modal.Content>
+            </Modal>
+            <hr />
+            <Button animated>
+              <Button.Content visible>Find</Button.Content>
+              <Button.Content hidden>
+                <Icon name='arrow down' />
+              </Button.Content>
+            </Button>
+            
+            
+            
+        </div> 
+    );
 
 };
 
 export default Travel;
-
