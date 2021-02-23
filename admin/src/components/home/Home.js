0ef74ad2e43c0b8c4328ab18721e2eba33a0edae
HmingPuia <sawmasn7@gmail.com>
2021-02-23 01:55:40 -0700

    Random Changes

--- admin/src/components/home/Home.js
+++ admin/src/components/home/Home.js
@@ -8,7 +8,7 @@ const Home=(param)=>{
             <div className="showcase">
                 <h2>Welcome to Railway Admin Page</h2>
                 <p>This portal allows administrators the ability to modify the database.</p>
-            {!param.isLoggedIn ? <><p>Only authorized personnel may access this page</p><Link to="/login">Please Login</Link></> : <p>Welcome, Administrator</p>}
+            {!param.isLoggedIn ? <><p>Only authorized personnel may access this page</p><Link style={{ textDecoration: 'none' }} to="/login">Please Login</Link></> : <p>Welcome, Administrator</p>}
             <Image src="./Train2.png" fluid style={{padding: '0 13%'}}/>
             </div>
             <p className="copy">&copy;2021 Railway</p>
